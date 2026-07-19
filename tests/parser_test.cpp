b43ced83045354c88defa3cc02e233d0c3a478c8
AYGA2k <garouatayoub@gmail.com>
2026-07-19 17:56:14 +0400

    add tests for parse binary

--- tests/parser_test.cpp
+++ tests/parser_test.cpp
@@ -15,11 +15,16 @@ std::vector<Token> tokenize(const std::string &input) {
   return lexer.tokenize();
 }
 
+Expression at(Parser &parser, int index) {
+  return parser.parserResult.expressions[index];
+}
+
 TEST(Parser, ParseIdentifier) {
   std::vector<Token> tokens = tokenize("foobar;");
   Parser parser{tokens};
 
-  Expression expression = parser.parseIdentifier();
+  int index = parser.parseIdentifier();
+  Expression expression = parser.parserResult.expressions[index];
 
   EXPECT_EQ(static_cast<int>(expression.kind),
             static_cast<int>(ExpressionKind::IDENTIFIER));
@@ -31,7 +36,8 @@ TEST(Parser, ParseNumberInteger) {
   std::vector<Token> tokens = tokenize("42;");
   Parser parser{tokens};
 
-  Expression expression = parser.parseNumber();
+  int index = parser.parseNumber();
+  Expression expression = parser.parserResult.expressions[index];
 
   EXPECT_EQ(static_cast<int>(expression.kind),
             static_cast<int>(ExpressionKind::LITERAL_NUMBER));
@@ -43,7 +49,8 @@ TEST(Parser, ParseNumberFloat) {
   std::vector<Token> tokens = tokenize("3.14;");
   Parser parser{tokens};
 
-  Expression expression = parser.parseNumber();
+  int index = parser.parseNumber();
+  Expression expression = parser.parserResult.expressions[index];
 
   EXPECT_EQ(static_cast<int>(expression.kind),
             static_cast<int>(ExpressionKind::LITERAL_NUMBER));
@@ -51,74 +58,82 @@ TEST(Parser, ParseNumberFloat) {
   EXPECT_TRUE(parser.errors.empty());
 }
 
-TEST(Parser, ParsePrefixBang) {
+TEST(Parser, ParseUnaryBang) {
   std::vector<Token> tokens = tokenize("!5;");
   Parser parser{tokens};
 
-  Expression expression = parser.parsePrefix();
+  int index = parser.parseUnary();
+  Expression expression = parser.parserResult.expressions[index];
 
   EXPECT_EQ(static_cast<int>(expression.kind),
-            static_cast<int>(ExpressionKind::PREFIX));
+            static_cast<int>(ExpressionKind::UNARY));
   EXPECT_EQ(static_cast<int>(expression.unaryOperator),
             static_cast<int>(UnaryOperator::NOT));
   ASSERT_GE(expression.operandExprIndex, 0);
-  Expression operand = parser.parserResult.expressions[expression.operandExprIndex];
+  Expression operand =
+      parser.parserResult.expressions[expression.operandExprIndex];
   EXPECT_EQ(static_cast<int>(operand.kind),
             static_cast<int>(ExpressionKind::LITERAL_NUMBER));
   EXPECT_DOUBLE_EQ(operand.numValue, 5.0);
   EXPECT_TRUE(parser.errors.empty());
 }
 
-TEST(Parser, ParsePrefixMinus) {
+TEST(Parser, ParseUnaryMinus) {
   std::vector<Token> tokens = tokenize("-15;");
   Parser parser{tokens};
 
-  Expression expression = parser.parsePrefix();
+  int index = parser.parseUnary();
+  Expression expression = parser.parserResult.expressions[index];
 
   EXPECT_EQ(static_cast<int>(expression.kind),
-            static_cast<int>(ExpressionKind::PREFIX));
+            static_cast<int>(ExpressionKind::UNARY));
   EXPECT_EQ(static_cast<int>(expression.unaryOperator),
             static_cast<int>(UnaryOperator::NEGATE));
   ASSERT_GE(expression.operandExprIndex, 0);
-  Expression operand = parser.parserResult.expressions[expression.operandExprIndex];
+  Expression operand =
+      parser.parserResult.expressions[expression.operandExprIndex];
   EXPECT_EQ(static_cast<int>(operand.kind),
             static_cast<int>(ExpressionKind::LITERAL_NUMBER));
   EXPECT_DOUBLE_EQ(operand.numValue, 15.0);
   EXPECT_TRUE(parser.errors.empty());
 }
 
-TEST(Parser, ParsePrefixBangIdentifier) {
+TEST(Parser, ParseUnaryBangIdentifier) {
   std::vector<Token> tokens = tokenize("!foobar;");
   Parser parser{tokens};
 
-  Expression expression = parser.parsePrefix();
+  int index = parser.parseUnary();
+  Expression expression = parser.parserResult.expressions[index];
 
   EXPECT_EQ(static_cast<int>(expression.kind),
-            static_cast<int>(ExpressionKind::PREFIX));
+            static_cast<int>(ExpressionKind::UNARY));
   EXPECT_EQ(static_cast<int>(expression.unaryOperator),
             static_cast<int>(UnaryOperator::NOT));
   ASSERT_GE(expression.operandExprIndex, 0);
-  Expression operand = parser.parserResult.expressions[expression.operandExprIndex];
+  Expression operand =
+      parser.parserResult.expressions[expression.operandExprIndex];
   EXPECT_EQ(static_cast<int>(operand.kind),
             static_cast<int>(ExpressionKind::IDENTIFIER));
   EXPECT_EQ(operand.stringValue, "foobar");
   EXPECT_TRUE(parser.errors.empty());
 }
 
-TEST(Parser, ParsePrefixNested) {
+TEST(Parser, ParseUnaryNested) {
   std::vector<Token> tokens = tokenize("!!5;");
   Parser parser{tokens};
 
-  Expression expression = parser.parsePrefix();
+  int index = parser.parseUnary();
+  Expression expression = parser.parserResult.expressions[index];
 
   EXPECT_EQ(static_cast<int>(expression.kind),
-            static_cast<int>(ExpressionKind::PREFIX));
+            static_cast<int>(ExpressionKind::UNARY));
   EXPECT_EQ(static_cast<int>(expression.unaryOperator),
             static_cast<int>(UnaryOperator::NOT));
   ASSERT_GE(expression.operandExprIndex, 0);
-  Expression inner = parser.parserResult.expressions[expression.operandExprIndex];
+  Expression inner =
+      parser.parserResult.expressions[expression.operandExprIndex];
   EXPECT_EQ(static_cast<int>(inner.kind),
-            static_cast<int>(ExpressionKind::PREFIX));
+            static_cast<int>(ExpressionKind::UNARY));
   EXPECT_EQ(static_cast<int>(inner.unaryOperator),
             static_cast<int>(UnaryOperator::NOT));
   ASSERT_GE(inner.operandExprIndex, 0);
@@ -133,13 +148,18 @@ TEST(Parser, IdentifierExpressionStatement) {
   std::vector<Token> tokens = tokenize("foobar;");
   Parser parser{tokens};
 
-  Statement statement = parser.parseExpressionStatment();
+  parser.parseExpressionStatment();
 
+  ASSERT_FALSE(parser.parserResult.statements.empty());
+  Statement statement = parser.parserResult.statements[0];
   EXPECT_EQ(static_cast<int>(statement.kind),
             static_cast<int>(StatementKind::EXPRESSION));
-  EXPECT_EQ(static_cast<int>(statement.expression.kind),
+  ASSERT_GE(statement.expressionIndex, 0);
+  Expression expression =
+      parser.parserResult.expressions[statement.expressionIndex];
+  EXPECT_EQ(static_cast<int>(expression.kind),
             static_cast<int>(ExpressionKind::IDENTIFIER));
-  EXPECT_EQ(statement.expression.stringValue, "foobar");
+  EXPECT_EQ(expression.stringValue, "foobar");
   EXPECT_TRUE(parser.errors.empty());
 }
 
@@ -147,13 +167,123 @@ TEST(Parser, NumberExpressionStatement) {
   std::vector<Token> tokens = tokenize("5;");
   Parser parser{tokens};
 
-  Statement statement = parser.parseExpressionStatment();
+  parser.parseExpressionStatment();
 
+  ASSERT_FALSE(parser.parserResult.statements.empty());
+  Statement statement = parser.parserResult.statements[0];
   EXPECT_EQ(static_cast<int>(statement.kind),
             static_cast<int>(StatementKind::EXPRESSION));
-  EXPECT_EQ(static_cast<int>(statement.expression.kind),
+  ASSERT_GE(statement.expressionIndex, 0);
+  Expression expression =
+      parser.parserResult.expressions[statement.expressionIndex];
+  EXPECT_EQ(static_cast<int>(expression.kind),
             static_cast<int>(ExpressionKind::LITERAL_NUMBER));
-  EXPECT_DOUBLE_EQ(statement.expression.numValue, 5.0);
+  EXPECT_DOUBLE_EQ(expression.numValue, 5.0);
+  EXPECT_TRUE(parser.errors.empty());
+}
+
+TEST(Parser, ParseBinaryAdd) {
+  std::vector<Token> tokens = tokenize("1 + 2;");
+  Parser parser{tokens};
+
+  parser.parseExpressionStatment();
+
+  ASSERT_FALSE(parser.parserResult.statements.empty());
+  Expression expression =
+      at(parser, parser.parserResult.statements[0].expressionIndex);
+  EXPECT_EQ(static_cast<int>(expression.kind),
+            static_cast<int>(ExpressionKind::BINARY));
+  EXPECT_EQ(static_cast<int>(expression.binaryOperator),
+            static_cast<int>(BinaryOperator::ADD));
+
+  Expression left = at(parser, expression.leftExprIndex);
+  Expression right = at(parser, expression.rightExprIndex);
+  EXPECT_DOUBLE_EQ(left.numValue, 1.0);
+  EXPECT_DOUBLE_EQ(right.numValue, 2.0);
+  EXPECT_TRUE(parser.errors.empty());
+}
+
+TEST(Parser, ParseBinaryProductBindsTighterThanSum) {
+  std::vector<Token> tokens = tokenize("1 + 2 * 3;");
+  Parser parser{tokens};
+
+  parser.parseExpressionStatment();
+
+  ASSERT_FALSE(parser.parserResult.statements.empty());
+  // Expect 1 + (2 * 3): root is ADD, right child is MULTIPLY.
+  Expression root =
+      at(parser, parser.parserResult.statements[0].expressionIndex);
+  EXPECT_EQ(static_cast<int>(root.kind),
+            static_cast<int>(ExpressionKind::BINARY));
+  EXPECT_EQ(static_cast<int>(root.binaryOperator),
+            static_cast<int>(BinaryOperator::ADD));
+
+  Expression left = at(parser, root.leftExprIndex);
+  EXPECT_DOUBLE_EQ(left.numValue, 1.0);
+
+  Expression right = at(parser, root.rightExprIndex);
+  EXPECT_EQ(static_cast<int>(right.kind),
+            static_cast<int>(ExpressionKind::BINARY));
+  EXPECT_EQ(static_cast<int>(right.binaryOperator),
+            static_cast<int>(BinaryOperator::MULTIPLY));
+  EXPECT_DOUBLE_EQ(at(parser, right.leftExprIndex).numValue, 2.0);
+  EXPECT_DOUBLE_EQ(at(parser, right.rightExprIndex).numValue, 3.0);
+  EXPECT_TRUE(parser.errors.empty());
+}
+
+TEST(Parser, ParseBinaryLeftAssociative) {
+  std::vector<Token> tokens = tokenize("1 - 2 - 3;");
+  Parser parser{tokens};
+
+  parser.parseExpressionStatment();
+
+  ASSERT_FALSE(parser.parserResult.statements.empty());
+  // Expect (1 - 2) - 3: root is SUBTRACT, left child is SUBTRACT.
+  Expression root =
+      at(parser, parser.parserResult.statements[0].expressionIndex);
+  EXPECT_EQ(static_cast<int>(root.kind),
+            static_cast<int>(ExpressionKind::BINARY));
+  EXPECT_EQ(static_cast<int>(root.binaryOperator),
+            static_cast<int>(BinaryOperator::SUBTRACT));
+
+  Expression right = at(parser, root.rightExprIndex);
+  EXPECT_DOUBLE_EQ(right.numValue, 3.0);
+
+  Expression left = at(parser, root.leftExprIndex);
+  EXPECT_EQ(static_cast<int>(left.kind),
+            static_cast<int>(ExpressionKind::BINARY));
+  EXPECT_EQ(static_cast<int>(left.binaryOperator),
+            static_cast<int>(BinaryOperator::SUBTRACT));
+  EXPECT_DOUBLE_EQ(at(parser, left.leftExprIndex).numValue, 1.0);
+  EXPECT_DOUBLE_EQ(at(parser, left.rightExprIndex).numValue, 2.0);
+  EXPECT_TRUE(parser.errors.empty());
+}
+
+TEST(Parser, ParseBinarySumBindsTighterThanComparison) {
+  std::vector<Token> tokens = tokenize("1 + 2 == 3;");
+  Parser parser{tokens};
+
+  parser.parseExpressionStatment();
+
+  ASSERT_FALSE(parser.parserResult.statements.empty());
+  // Expect (1 + 2) == 3: root is EQUAL, left child is ADD.
+  Expression root =
+      at(parser, parser.parserResult.statements[0].expressionIndex);
+  EXPECT_EQ(static_cast<int>(root.kind),
+            static_cast<int>(ExpressionKind::BINARY));
+  EXPECT_EQ(static_cast<int>(root.binaryOperator),
+            static_cast<int>(BinaryOperator::EQUAL));
+
+  Expression right = at(parser, root.rightExprIndex);
+  EXPECT_DOUBLE_EQ(right.numValue, 3.0);
+
+  Expression left = at(parser, root.leftExprIndex);
+  EXPECT_EQ(static_cast<int>(left.kind),
+            static_cast<int>(ExpressionKind::BINARY));
+  EXPECT_EQ(static_cast<int>(left.binaryOperator),
+            static_cast<int>(BinaryOperator::ADD));
+  EXPECT_DOUBLE_EQ(at(parser, left.leftExprIndex).numValue, 1.0);
+  EXPECT_DOUBLE_EQ(at(parser, left.rightExprIndex).numValue, 2.0);
   EXPECT_TRUE(parser.errors.empty());
 }
 
